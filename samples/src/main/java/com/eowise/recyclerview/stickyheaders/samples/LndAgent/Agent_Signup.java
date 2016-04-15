d2358f401bd42a471c1fcdf056e71bc3ad93f19c
lastnightdress <bisht.sanjaysingh97@gmail.com>
2016-04-15 16:47:58 +0530

    agent activity added

---
+++ samples/src/main/java/com/eowise/recyclerview/stickyheaders/samples/LndAgent/Agent_Signup.java
@@ -0,0 +1,15 @@
+package com.eowise.recyclerview.stickyheaders.samples.LndAgent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eowise.recyclerview.stickyheaders.samples.R;
+
+public class Agent_Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agent__signup);
+    }
+}
